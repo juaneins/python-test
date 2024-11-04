879ea49af4b24ff3484463a1ccc6ad56714cb822
juaneins_uio <ballesterosjg@gmail.com>
2024-11-04 12:50:23 -0500

    se crea metodo teardown para liberar recursos de pruebas

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,11 +1,19 @@
 import unittest
+import os
 from src.bank_account import BankAccount
 
 class BankAccountTest(unittest.TestCase):
 
     def setUp(self):
-        self.account = BankAccount(balance=1000)
+        self.account = BankAccount(balance=1000, log_file="transaction_log.txt")
 
+    def tearDown(self):
+        if os.path.exists(self.account.log_file):
+            os.remove(self.account.log_file)
+
+    def _count_lines(self, filename):
+        with open(filename, "r") as f:
+            return len(f.readlines())
 
     def test_deposit(self):
         #account = BankAccount(balance=1000)
@@ -21,3 +29,12 @@ class BankAccountTest(unittest.TestCase):
     def test_get_balance(self):
         #account = BankAccount(balance=1000)
         self.assertEqual(self.account.get_balance(), 1000)
+
+    def test_transaction_log(self):
+        self.account.deposit(500)        
+        self.assertTrue(os.path.exists("transaction_log.txt"))
+
+    def test_count_transactions(self):
+        assert self._count_lines(self.account.log_file) == 1
+        self.account.deposit(500)  
+        assert self._count_lines(self.account.log_file) == 2

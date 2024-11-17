8a79d8f8522367f279a5ff1235e0189cfba41762
juaneins_uio <ballesterosjg@gmail.com>
2024-11-17 17:37:18 -0500

    Uso de Patching para Modificar Comportamientos en Python

--- tests/test_bank_account.py
+++ tests/test_bank_account.py
@@ -1,7 +1,8 @@
 import unittest
 import os
+from unittest.mock import patch
 from src.bank_account import BankAccount
-from src.exceptions import InsufficientFundsError
+from src.exceptions import InsufficientFundsError, WithdrawalTimeRestrictionError
 
 class BankAccountTest(unittest.TestCase):
 
@@ -39,3 +40,21 @@ class BankAccountTest(unittest.TestCase):
     def test_withdraw_raises_error_when_insufficient_funds(self):
         with self.assertRaises(InsufficientFundsError):
             self.account.withdraw(2000)
+
+    @patch("src.bank_account.datetime")
+    def test_withraw_during_bussines_hours(self, mock_datetime):
+        mock_datetime.now.return_value.hour = 10
+        new_balance = self.account.withdraw(100)
+        self.assertEqual(new_balance, 900)
+
+    @patch("src.bank_account.datetime")
+    def test_withraw_disallow_before_bussines_hours(self, mock_datetime):
+        mock_datetime.now.return_value.hour = 7
+        with self.assertRaises(WithdrawalTimeRestrictionError):
+            self.account.withdraw(100)  
+
+    @patch("src.bank_account.datetime")
+    def test_withraw_disallow_after_bussines_hours(self, mock_datetime):
+        mock_datetime.now.return_value.hour = 18
+        with self.assertRaises(WithdrawalTimeRestrictionError):
+            self.account.withdraw(100)                                  
